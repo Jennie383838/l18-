147e2a2cdfcfc7054ed6e445bf48cbf7bb65144d
Xavier Thong <xavierthong4@gmail.com>
2026-01-15 15:34:28 +0800

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import About from "./components/About";
 import Diplomas from "./components/Diplomas";
 import Diploma from "./components/Diploma";
 import Module from "./components/Module";
