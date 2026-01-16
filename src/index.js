5bae6a53a67a671c31d13524ba98e61aca72e623
Xavier Thong <xavierthong4@gmail.com>
2026-01-16 15:00:08 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
-import "./index.css";
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
